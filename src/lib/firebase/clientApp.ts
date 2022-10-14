5e2e9911ea925ecc9fff05dfae6f9b9a3d3ed2ad
Icaro Oliveira <icarovinici@gmail.com>
2022-10-14 12:56:33 -0300

    chore: integrates with firebase

---
+++ src/lib/firebase/clientApp.ts
@@ -0,0 +1,22 @@
+import { getApp, getApps, initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
+
+import { FIREBASE } from '@src/utils/constants'
+
+const firebaseConfig = {
+  apiKey: FIREBASE.FIREBASE_API_KEY,
+  authDomain: FIREBASE.FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE.FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE.FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE.FIREBASE_APP_ID
+}
+
+// Initialize Firebase for Server Side Rendering
+export const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
+
+export const firestore = getFirestore(app)
+export const auth = getAuth(app)
+export const storage = getStorage(app)
