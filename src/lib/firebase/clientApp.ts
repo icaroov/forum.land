189eb5856ae122c69c7aa801a140283639d06a85
Icaro Oliveira <icarovinici@gmail.com>
2022-10-22 02:02:30 -0300

    fix: adjust import

--- src/lib/firebase/clientApp.ts
+++ src/lib/firebase/clientApp.ts
@@ -3,7 +3,7 @@ import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
 
-import { FIREBASE } from '@src/utils/constants'
+import { FIREBASE } from '@utils/constants'
 
 const firebaseConfig = {
   apiKey: FIREBASE.FIREBASE_API_KEY,
