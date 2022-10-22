95e22cccee79b30a1be323f5d14a27237f596bb7
Icaro Oliveira <icarovinici@gmail.com>
2022-10-22 00:37:34 -0300

    feat: adds NavbarIcons component

--- src/components/Layout/Navbar/NavbarIcons.tsx
+++ src/components/Layout/Navbar/NavbarIcons.tsx
@@ -1,5 +1,8 @@
-import { Flex } from '@chakra-ui/react'
+import { Flex, Icon } from '@chakra-ui/react'
+import { IconType } from 'react-icons'
+import { BsArrowRightCircle, BsChatDots } from 'react-icons/bs'
 import {
+  IoAddOutline,
   IoFilterCircleOutline,
   IoNotificationsCircleOutline,
   IoVideocamOutline
@@ -8,11 +11,57 @@ import {
 const ICONS = {
   FILTER: IoFilterCircleOutline,
   NOTIFICATIONS: IoNotificationsCircleOutline,
-  VIDEO: IoVideocamOutline
+  VIDEO: IoVideocamOutline,
+  CHAT: BsChatDots,
+  ARROW: BsArrowRightCircle,
+  ADD: IoAddOutline
+}
+
+const SingleIcon = ({
+  icon,
+  size,
+  hideOnMobile = false
+}: {
+  icon: IconType
+  size: number
+  hideOnMobile?: boolean
+}) => {
+  return (
+    <Flex
+      display={{ base: hideOnMobile ? 'none' : 'flex', md: 'flex' }}
+      marginX={1.5}
+      padding={1}
+      cursor="pointer"
+      borderRadius={4}
+      _hover={{ bg: 'pink.400' }}
+      color="gray.200"
+    >
+      <Icon as={icon} fontSize={size} />
+    </Flex>
+  )
 }
 
 const NavbarIcons = () => {
-  return <Flex></Flex>
+  return (
+    <Flex>
+      <Flex
+        display={{ base: 'none', md: 'flex' }}
+        align="center"
+        borderRight="1px solid"
+        borderColor="gray.600"
+      >
+        <SingleIcon icon={ICONS.ARROW} size={20} />
+        <SingleIcon icon={ICONS.FILTER} size={22} />
+        <SingleIcon icon={ICONS.VIDEO} size={22} />
+      </Flex>
+
+      <>
+        <SingleIcon icon={ICONS.CHAT} size={20} />
+        <SingleIcon icon={ICONS.NOTIFICATIONS} size={20} />
+        <SingleIcon icon={ICONS.ADD} size={22} hideOnMobile />
+      </>
+    </Flex>
+  )
 }
 
 export default NavbarIcons
--- src/components/Layout/Navbar/RightContent.tsx
+++ src/components/Layout/Navbar/RightContent.tsx
@@ -1,39 +1,18 @@
-import { Button, Flex } from '@chakra-ui/react'
-import { signOut } from 'firebase/auth'
-import { useTranslation } from 'next-i18next'
+import { Flex } from '@chakra-ui/react'
 import { useAuthState } from 'react-firebase-hooks/auth'
 
 import AuthButtons from '@src/components/auth/AuthButtons'
-import UserAvatar from '@src/components/common/UserAvatar'
 
 import { auth } from '@lib/firebase/clientApp'
 
+import NavbarIcons from './NavbarIcons'
+
 const RightContent = () => {
   const [user] = useAuthState(auth)
-  const { t } = useTranslation('auth')
-
-  const trans = {
-    logout: t('logout.title')
-  }
 
   return (
     <Flex justify="center" align="center" gap={2}>
-      {user ? (
-        <>
-          <UserAvatar user={user} />
-
-          <Button
-            width={user.displayName ? 'unset' : '100px'}
-            height="28px"
-            variant="outline"
-            onClick={() => signOut(auth)}
-          >
-            {trans.logout}
-          </Button>
-        </>
-      ) : (
-        <AuthButtons />
-      )}
+      {user ? <NavbarIcons /> : <AuthButtons />}
     </Flex>
   )
 }
