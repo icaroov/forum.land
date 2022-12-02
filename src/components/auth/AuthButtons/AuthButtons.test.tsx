2c9dbb3d37a4c69ac58e37b28d58b931660ffd8d
Icaro Oliveira <icarovinici@gmail.com>
2022-12-02 13:50:30 -0300

    test: adds AuthButtons test

---
+++ src/components/auth/AuthButtons/AuthButtons.test.tsx
@@ -0,0 +1,65 @@
+import { fireEvent, screen } from '@testing-library/react'
+
+import { authModalAtom } from '@src/atoms/authModalAtom'
+import { renderWithProviders } from '@src/config/test/renderWithProviders'
+
+import AuthButtons from '.'
+
+jest.mock('next-i18next', () => ({
+  ...jest.requireActual('next-i18next'),
+  useTranslation: () => {
+    return {
+      t: (str: string) => str,
+      i18n: {
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
+        changeLanguage: () => new Promise(() => {})
+      }
+    }
+  }
+}))
+
+describe('<AuthButtons />', () => {
+  renderWithProviders(<AuthButtons />)
+
+  it('should renders correctly', () => {
+    const loginButton = screen.getByRole('button', { name: 'login.title' })
+    const registerButton = screen.getByRole('button', {
+      name: 'register.title'
+    })
+
+    expect(loginButton).toBeInTheDocument()
+    expect(registerButton).toBeInTheDocument()
+  })
+
+  it("should switch to 'register' form when 'register' button is clicked", () => {
+    const onClick = jest.fn()
+
+    renderWithProviders(<AuthButtons />, {
+      node: authModalAtom,
+      handler: onClick
+    })
+
+    const registerButton = screen.getByRole('button', {
+      name: 'register.title'
+    })
+
+    fireEvent.click(registerButton)
+
+    expect(onClick).toBeCalledWith({ isOpen: true, view: 'register' })
+  })
+
+  it("should switch to 'login' form when 'login' button is clicked", () => {
+    const onClick = jest.fn()
+
+    renderWithProviders(<AuthButtons />, {
+      node: authModalAtom,
+      handler: onClick
+    })
+
+    const loginButton = screen.getByRole('button', { name: 'login.title' })
+
+    fireEvent.click(loginButton)
+
+    expect(onClick).toBeCalledWith({ isOpen: true, view: 'login' })
+  })
+})
