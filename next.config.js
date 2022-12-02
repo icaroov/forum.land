8d4e80cde7efd9a4ecd8c5e29cab77e5f32e4d25
Icaro Oliveira <icarovinici@gmail.com>
2022-12-02 13:49:49 -0300

    chore: improves jest config with report file

--- next.config.js
+++ next.config.js
@@ -1,28 +1,21 @@
+/**
+ * @type {import('next').NextConfig}
+ */
 /* eslint-disable @typescript-eslint/no-var-requires */
-/** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')
 const { i18n } = require('./next-i18next.config.js')
 
-const isProd = process.env.NODE_ENV === 'production'
+const nextConfig = {
+  i18n,
+  reactStrictMode: true,
+  webpack: config => {
+    config.resolve.fallback = { fs: false }
 
-const nextConfig = () =>
-  withPWA({
-    i18n,
-    reactStrictMode: true,
-    webpack5: true,
-    webpack: config => {
-      config.resolve.fallback = { fs: false }
-
-      return config
-    },
-    pwa: {
-      dest: 'public',
-      disable: !isProd
-    },
-    images: {
-      domains: ['https://lh3.googleusercontent.com'],
-      minimumCacheTTL: 2_630_000
-    }
-  })
+    return config
+  },
+  images: {
+    domains: ['https://lh3.googleusercontent.com'],
+    minimumCacheTTL: 2_630_000
+  }
+}
 
 module.exports = nextConfig
